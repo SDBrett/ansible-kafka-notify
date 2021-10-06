e2c59816eac132829a74b2ff3e94a3754c685c54
Brett Johnson <brett@sdbrett.com>
2021-10-06 13:16:50 +1100

    added documentation
    Signed-off-by: Brett Johnson <brett@sdbrett.com>

--- library/kafka_notify.py
+++ library/kafka_notify.py
@@ -1,33 +1,69 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from __future__ import (absolute_import, division, print_function)
+# Copyright: (c) 2021, Brett Johnson <brett@sdbrett.com>
+# MIT License
 
+from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-DOCUMENTATION = """
-module = kafka_notify
+DOCUMENTATION = r'''
+---
+module: kafka_notify
 short_description: Send Messages to Kafka
+version_added: "1.0.0"
 description: Produces new messages to Kafka cluster
-author: "Brett Johnson (@sdbrett)"
+author: 
+  - "Brett Johnson (@sdbrett)"
 options:
-	topic:
-		type: str
-		description:
-			- The topic name to publish the message to
-	producer_config:
-		type: dict
-		elements: str
-		description:
-			- producer configuration parameters
-			- The full list of configuration parameters are available here: https://kafka-python.readthedocs.io/en/master/apidoc/KafkaProducer.html
-		required: true
-	msg:
-		type: str
-		description:
-			- Message to send
-version_added: 1.0.0
-"""
+  topic:
+    type: str
+    description:
+    - The topic name to publish the message to
+  producer_config:
+    type: dict
+    elements: str
+    description:
+    - producer configuration parameters
+    - The full list of configuration parameters are available at https://kafka-python.readthedocs.io/en/master/apidoc/KafkaProducer.html/
+    required: true
+  msg:
+    type: str
+    description:
+    - Message to send
+'''
+
+EXAMPLES = r'''
+# Produce simple message
+- name: Produce message
+  kafka_notify:
+    producer_config:
+      bootstrap_servers: 'localhost:9092'
+    msg: "my test message"
+    topic: testTopic
+    
+# Use as a handler to send task results
+- hosts: localhost
+  vars:
+    topic: testTopic
+    client_properties:
+      bootstrap_servers: 'localhost:9092'
+
+  tasks:
+    - name: create file
+      file:
+        path: ~/git-repos/ansible-kafka-notify/testFile
+        state: touch
+      register: msg
+      notify: test
+
+  handlers:
+    - name: test
+      kafka_notify:
+        producer_config: "{{ client_properties }}"
+        msg: "{{ msg }}"
+        topic: "{{ topic }}"
+'''
 
 from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 import traceback
