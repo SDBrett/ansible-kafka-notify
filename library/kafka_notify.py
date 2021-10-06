e0da6f2dbf14dc4a24edcccd6d58809d3adc2e4b
Brett Johnson <brett@sdbrett.com>
2021-10-07 09:25:35 +1100

    added params for ssl and sasl passwords
    Signed-off-by: Brett Johnson <brett@sdbrett.com>

--- library/kafka_notify.py
+++ library/kafka_notify.py
@@ -5,6 +5,7 @@
 # MIT License
 
 from __future__ import (absolute_import, division, print_function)
+
 __metaclass__ = type
 
 DOCUMENTATION = r'''
@@ -27,6 +28,16 @@ options:
     - producer configuration parameters
     - The full list of configuration parameters are available at https://kafka-python.readthedocs.io/en/master/apidoc/KafkaProducer.html/
     required: true
+  ssl_password:
+    type: str
+    description:
+    - Password for certificate chain file
+    - Has priority over producer_config['ssl_password'] 
+  sasl_plain_password:
+    type: str
+    description:
+    - Password for sasl PLAIN and SCRAM authentication
+    - Has priority over producer_config['sasl_plain_password'] 
   msg:
     type: str
     description:
@@ -69,38 +80,49 @@ from ansible.module_utils.basic import AnsibleModule, missing_required_lib
 import traceback
 
 try:
-	from kafka import KafkaProducer
+    from kafka import KafkaProducer
 except ImportError:
-	HAS_ANOTHER_LIBRARY = False
-	ANOTHER_LIBRARY_IMPORT_ERROR = traceback.format_exc()
+    HAS_ANOTHER_LIBRARY = False
+    ANOTHER_LIBRARY_IMPORT_ERROR = traceback.format_exc()
 else:
-	HAS_ANOTHER_LIBRARY = True
+    HAS_ANOTHER_LIBRARY = True
 
 
 def main():
-	module = AnsibleModule(
-		argument_spec=dict(
-			topic=dict(type='str'),
-			producer_config=dict(type='dict', required=True, no_log=False),
-			msg=dict(type='str'),
-		),
-		supports_check_mode=True,
-	)
+    module = AnsibleModule(
+        argument_spec=dict(
+            topic=dict(type='str'),
+            producer_config=dict(type='dict', required=True, no_log=False),
+            ssl_password=dict(type='str', no_log=True),
+            sasl_plain_password=dict(type=str, no_log=True),
+            msg=dict(type='str'),
+        ),
+        supports_check_mode=True,
+    )
 
-	if not HAS_ANOTHER_LIBRARY:
-		module.fail_json(
-			msg=missing_required_lib('kafka'),
-			exception=ANOTHER_LIBRARY_IMPORT_ERROR)
+    if not HAS_ANOTHER_LIBRARY:
+        module.fail_json(
+            msg=missing_required_lib('kafka'),
+            exception=ANOTHER_LIBRARY_IMPORT_ERROR)
 
-	topic = module.params['topic']
-	producer_config = module.params['producer_config']
-	msg = module.params['msg']
+    topic = module.params['topic']
+    producer_config = module.params['producer_config']
+    msg = module.params['msg']
 
-	producer = KafkaProducer(**producer_config)
-	producer.send(topic, msg.encode())
-	producer.flush()
-	producer.close()
-	module.exit_json(msg="OK")
+    if producer_config['sasl_mechanism'] in ('PLAIN', 'SCRAM-SHA-256', 'SCRAM-SHA-512'):
+        if module.params['sasl_plain_password'] is not None:
+            producer_config['sasl_plain_password'] = module.params['sasl_plain_password']
+
+    if producer_config['security_protocol'] in ('SSL', 'SASL_SSL'):
+        if module.params['ssl_password'] is not None:
+            producer_config['ssl_password'] = module.params['ssl_password']
+
+    producer = KafkaProducer(**producer_config)
+    producer.send(topic, msg.encode())
+    producer.flush()
+    producer.close()
+    module.exit_json(msg="OK")
+
 
 if __name__ == "__main__":
-	main()
+    main()
